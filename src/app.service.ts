ed8229c71e6e311255c6a5eb905b0db6576c9e31
Deepchand Prajapati <prajapatideepchand12@gmail.com>
2024-01-25 23:25:44 +0530

    first commit

---
+++ src/app.service.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  getSomething(): string {
+    return 'Something can sent back';
+  }
+}
